8fb176d55256f097d99ad3e448e388ab1cec19be
Saifur <saifurrahmany43@gmail.com>
2018-12-22 00:25:07 +0600

    pagination and about

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'Price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor', 'list_date', 'Price')
+    list_editable = ('is_published',)
+    search_fields = ('id', 'title', 'is_published', 'Price', 'list_date', 'realtor')
+    list_per_page = 20
+    ordering = ('id',)
+
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email', 'hire_date', 'is_mvp')
+    list_display_links = ('id', 'name')
+    list_filter = ('name', 'hire_date')
+    list_editable = ('is_mvp',)
+    search_fields = ('id', 'name', 'phone', 'email', 'hire_date', 'is_mvp')
+    list_per_page = 20
+
+
+admin.site.register(Realtor, RealtorAdmin)
